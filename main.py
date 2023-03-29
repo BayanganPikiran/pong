341659bdf7ea2056b1abe835284538999ab50e64
Andre LaFlamme <bayangan.pikiran@gmail.com>
2023-03-29 09:08:56 +0700

    paddle movement added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import time
 from turtle import Screen
 from paddle import Paddle
+
 
 SCREEN_AZURE = "#F0FFFF"
 PADDLE1_GREEN = "#06FF00"
@@ -9,12 +11,19 @@ screen = Screen()
 screen.bgcolor(SCREEN_AZURE)
 screen.setup(width=800)
 screen.title("Ping-Pong Ding-Dong")
+screen.listen()
+
 
 paddle1 = Paddle()
 paddle1.color(PADDLE1_GREEN)
 paddle1.color()
 paddle2 = Paddle()
 paddle2.color(PADDLE2_RED)
+
+screen.onkey(paddle1.paddle_up, "w")
+screen.onkey(paddle1.paddle_down, "x")
+screen.onkey(paddle2.paddle_up, "Up")
+screen.onkey(paddle2.paddle_down, "Down")
 
 paddle1.goto(-375, 0)
 paddle2.goto(375, 0)
@@ -23,7 +32,7 @@ paddle2.goto(375, 0)
 
 screen.exitonclick()
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- paddle.py
+++ paddle.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
 
+MOVE_DISTANCE = 10
 
 class Paddle(Turtle):
 
@@ -10,3 +11,16 @@ class Paddle(Turtle):
         self.shapesize(stretch_len=5)
         self.setheading(90)
         self.speed('fastest')
+
+    def paddle_up(self):
+        self.forward(MOVE_DISTANCE)
+
+    def paddle_down(self):
+        self.backward(MOVE_DISTANCE)
+
+    # def paddle2_up(self):
+    #     self.forward(MOVE_DISTANCE)
+    #
+    # def paddle2_down(self):
+    #     self.backward(MOVE_DISTANCE)
+
